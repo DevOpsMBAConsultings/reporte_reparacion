56a9f3b72c44d3aa8a65b8d3ccff4ea2f09b2a76
MBA Consultings <devops@mbaconsultings.com>
2026-02-14 19:41:04 -0500

    fixes varios

--- models/repair_order.py
+++ models/repair_order.py
@@ -16,6 +16,40 @@ class RepairOrder(models.Model):
     
     vehicle_id = fields.Many2one('fleet.vehicle', string='Vehículo a reparar')
 
+    # Campos de cliente en modo lectura
+    partner_email = fields.Char(string='Correo', compute='_compute_partner_info', readonly=True)
+    partner_street = fields.Char(string='Dirección', compute='_compute_partner_info', readonly=True)
+    partner_phone = fields.Char(string='Teléfono', compute='_compute_partner_info', readonly=True)
+
+    # Campos de vehículo en modo lectura
+    vehicle_brand = fields.Char(string='Marca', compute='_compute_vehicle_info', readonly=True)
+    vehicle_vin = fields.Char(string='Número de Chassis', compute='_compute_vehicle_info', readonly=True)
+    vehicle_plate = fields.Char(string='Placa', compute='_compute_vehicle_info', readonly=True)
+
+    @api.depends('partner_id')
+    def _compute_partner_info(self):
+        for record in self:
+            if record.partner_id:
+                record.partner_email = record.partner_id.email
+                record.partner_street = record.partner_id.street
+                record.partner_phone = record.partner_id.phone
+            else:
+                record.partner_email = False
+                record.partner_street = False
+                record.partner_phone = False
+
+    @api.depends('vehicle_id')
+    def _compute_vehicle_info(self):
+        for record in self:
+            if record.vehicle_id:
+                record.vehicle_brand = record.vehicle_id.brand_id.name if record.vehicle_id.brand_id else ''
+                record.vehicle_vin = record.vehicle_id.vin_sn
+                record.vehicle_plate = record.vehicle_id.license_plate
+            else:
+                record.vehicle_brand = False
+                record.vehicle_vin = False
+                record.vehicle_plate = False
+
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
         if self.partner_id:
